7ca4cc7532c4cbc09f68f7c0a87a869f78b5121a
Laptop <Laptop@DESKTOP-FKIMTT1>
2023-06-29 22:39:42 +0200

    Dodaj pliki projektów.

---
+++ ToDoList/Services/ToDoListService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ToDoList.DataModel;
+
+namespace ToDoList.Services
+{
+    public class ToDoListService
+    {
+        public IEnumerable<ToDoItem> GetItems() => new[]
+        {
+            new ToDoItem { Description = "Projekty" },
+            new ToDoItem { Description = "Zaliczenia" },
+            new ToDoItem { Description = "Egazminy" },
+        };
+    }
+}
